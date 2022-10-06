02ed9a7e833390c00758c8c22f5d33260318909d
unknown <andreyvasilchenko777@mail.ru>
2022-10-07 01:37:44 +0300

    278. First Bad Version

---
+++ 278. First Bad Version/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _278._First_Bad_Version;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().FirstBadVersion(1));
+    }
+}
---
+++ 278. First Bad Version/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace _278._First_Bad_Version;
+
+public class VersionControl
+{
+    readonly int version = 1;
+    public bool IsBadVersion(int n)
+    {
+        return version <= n;
+    }
+}
+
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        long lowest = 0;
+        long highest = n;
+
+        while (highest >= lowest & n > 1)
+        {
+            long middle = (lowest + highest) / 2;
+
+            if (IsBadVersion( (int)(middle + 1) ))
+                highest = middle - 1;
+            else
+                lowest = middle + 1;
+        }
+        return (int)(lowest + 1);
+    }
+}
