eac17aa9fb2b5dfe54c42890e5631f5cd32f010e
unknown <andreyvasilchenko777@mail.ru>
2022-10-06 22:41:32 +0300

    704. Binary search

---
+++ 704. Binary Search/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _704._Binary_Search;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().Search(new int[] { 0, 7, 8, 10 }, 7));
+    }
+}
---
+++ 704. Binary Search/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace _704._Binary_Search;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int highest = nums.Length - 1;
+        int lowest = 0;
+
+        while (highest >= lowest)
+        {
+            int middle = (lowest + highest) / 2;
+
+            if (target == nums[middle])
+                return middle;
+
+            if (target < nums[middle])
+                highest = middle - 1;
+            else if (target > nums[middle])
+                lowest = middle + 1;
+        }
+        return -1;
+    }
+}
