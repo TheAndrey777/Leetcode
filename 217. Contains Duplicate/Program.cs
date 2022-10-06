0850461e0be1278274635facc92b9cb479a90d4f
unknown <andreyvasilchenko777@mail.ru>
2022-10-07 02:14:28 +0300

    217. Contains Duplicate

---
+++ 217. Contains Duplicate/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _217._Contains_Duplicate;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1, 2, 3, 1 }));
+    }
+}
---
+++ 217. Contains Duplicate/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace _217._Contains_Duplicate;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> numSet = new HashSet<int>(nums);
+        return !(numSet.Count == nums.Length);
+    }
+}
