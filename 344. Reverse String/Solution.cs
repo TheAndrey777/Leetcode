c705ee4200bc1d159b6de234cb54e946f40d38fd
unknown <andreyvasilchenko777@mail.ru>
2022-10-12 04:15:29 +0300

    557. Reverse Words in a String III

--- 344. Reverse String/Solution.cs
+++ 344. Reverse String/Solution.cs
@@ -4,9 +4,10 @@ public class Solution
 {
     public void ReverseString(char[] s)
     {
+        char temp;
         for (int i = 0; i <= (s.Length - 2) / 2; i++)
         {
-            char temp = s[i];
+            temp = s[i];
             s[i] = s[s.Length - 1 - i];
             s[s.Length - 1 - i] = temp;
         }
---
+++ 557. Reverse Words in a String III/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace _557._Reverse_Words_in_a_String_III;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        char[] c = s.ToCharArray();
+        char temp;
+
+        for (int i = 0; i <= (s.Length - 2) / 2; i++)
+        {
+            temp = c[i];
+            c[i] = c[s.Length - 1 - i];
+            c[c.Length - 1 - i] = temp;
+        }
+
+        string[] words = new string(c).Split(' ');
+        Array.Reverse(words);
+
+        return String.Join(" ", words);
+    }
+}
