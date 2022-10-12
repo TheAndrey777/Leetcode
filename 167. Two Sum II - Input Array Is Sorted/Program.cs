b6484f33938add7baf95990ad8ca4b90dc9fdd3c
unknown <andreyvasilchenko777@mail.ru>
2022-10-12 03:41:39 +0300

    167. Two Sum II - Input Array Is Sorted

---
+++ 167. Two Sum II - Input Array Is Sorted/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _167._Two_Sum_II___Input_Array_Is_Sorted;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(String.Join(" ", new Solution().TwoSum(new int[] { 0, 0, 3, 4 }, 0)));
+    }
+}
---
+++ 167. Two Sum II - Input Array Is Sorted/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace _167._Two_Sum_II___Input_Array_Is_Sorted;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int[] result = new int[2];
+
+        for (int i = 0; i <= numbers.Length - 1; i++)
+        {
+            int index = Array.BinarySearch(numbers, target - numbers[i]);
+            Console.WriteLine(index);
+            if (index >= 0)
+            {
+                result[0] = Math.Min(i + 1, index + 1);
+                result[1] = Math.Max(i + 1, index + 1);
+
+                if (result[0] != result[1])
+                    return result;
+            }
+        }
+        return result;
+    }
+}
