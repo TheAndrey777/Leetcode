6b1a87652460b006cb6d7e1e58e8a65b0190a32a
unknown <andreyvasilchenko777@mail.ru>
2022-10-12 03:56:38 +0300

    344. Reverse String

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _344._Reverse_String;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        new Solution().ReverseString(new char[] { 'a', 'b', 'c' });
+    }
+}
---
+++ 344. Reverse String/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace _344._Reverse_String;
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        for (int i = 0; i <= (s.Length - 2) / 2; i++)
+        {
+            char temp = s[i];
+            s[i] = s[s.Length - 1 - i];
+            s[s.Length - 1 - i] = temp;
+        }
+    }
+}
